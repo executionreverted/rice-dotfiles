bdc3894ed64f366e16d05a286605e589eabf1646
executionreverted <canersevince3@gmail.com>
2025-02-22 00:44:58 +0300

    nvim and doom

---
+++ doom/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/theena/Documents/Github/oneplus7git/firstorg.org" "/Users/theena/Documents/Github/oneplus7git/Getting Started with Orgzly.org" "/Users/theena/Documents/Github/oneplus7git/TODO.org" "/Users/theena/Documents/Github/oneplus7git/miscellaneous.org" "/Users/theena/Documents/Github/oneplus7git/misscellaneous.org" "/Users/theena/Documents/Github/oneplus7git/random.org"))
+ '(package-selected-packages '(evil-snipe xah-fly-keys)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
