be686a5ed949d4fc0ae4434c991e4a75cc2fe419
executionreverted <canersevince3@gmail.com>
2025-03-08 00:13:56 +0300

    lualine theme and macro view fix

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,155 @@
+local noice = require("noice")
+noice.setup({
+  event = "VeryLazy",
+  opts = {
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          any = {
+            { find = "%d+L, %d+B" },
+            { find = "; after #%d+" },
+            { find = "; before #%d+" },
+          },
+        },
+        view = "mini",
+      },
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "<leader>sn", "", desc = "+noice"},
+    { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+    { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+    { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+    { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+    { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+    { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+    { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+    { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+  },
+  config = function(_, opts)
+    if vim.o.filetype == "lazy" then
+      vim.cmd([[messages clear]])
+    end
+    require("noice").setup(opts)
+  end,
+
+  routes = {
+    {
+      view = "notify",
+      filter = { event = "msg_showmode" },
+    },
+  },
+})
+local function show_macro_recording()
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == "" then
+    return ""
+  else
+    return "Recording @" .. recording_register
+  end
+end
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        mode = { "showmode" },
+        theme = {
+          normal = {
+            a = { fg = "#14091e", bg = "#CE67F0", gui = "bold" },
+            b = { fg = "#E1D6F8", bg = "#201430" },
+            c = { fg = "#A4D2EC", bg = "#14091e" },
+          },
+          insert = {
+            a = { fg = "#14091e", bg = "#87ff5f", gui = "bold" },
+          },
+          visual = {
+            a = { fg = "#14091e", bg = "#DB6088", gui = "bold" },
+          },
+          replace = {
+            a = { fg = "#14091e", bg = "#D99145", gui = "bold" },
+          },
+          command = {
+            a = { fg = "#14091e", bg = "#E6BB85", gui = "bold" },
+          },
+          inactive = {
+            a = { fg = "#875faf", bg = "#14091e" },
+            b = { fg = "#9DAFD1", bg = "#14091e" },
+            c = { fg = "#3B3847", bg = "#14091e" },
+          },
+        },
+
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        extensions = { "neo-tree", "lazy", "fzf" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          {
+            "macro-recording",
+            fmt = show_macro_recording,
+          },
+        },
+        lualine_y = {
+          {
+            function()
+              return require("noice").api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.command.has()
+            end,
+            color = function()
+              return { fg = Snacks.util.color("Statement") }
+            end,
+          },
+          {
+            function()
+              return require("noice").api.status.mode.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.mode.has()
+            end,
+            color = function()
+              return { fg = Snacks.util.color("Constant") }
+            end,
+          },
+          {
+            function()
+              return "  " .. require("dap").status()
+            end,
+            cond = function()
+              return package.loaded["dap"] and require("dap").status() ~= ""
+            end,
+            color = function()
+              return { fg = Snacks.util.color("Debug") }
+            end,
+          },
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = function()
+              return { fg = Snacks.util.color("Special") }
+            end,
+          },
+        },
+      },
+    })
+  end,
+}
